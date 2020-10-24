320716d266869627abe60f3817ecb9c1db2a2bda
Gaut9a8m <gaut9a8m@gmail.com>
2020-10-24 16:58:04 +0530

    new changes to export files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {TodoserviceService} from './todoservice.service';
+import * as XLSX from 'xlsx';
+
+import {AngularFirestore} from '@angular/fire/firestore';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,29 +11,62 @@ export class AppComponent implements OnInit {
   title = 'my-todo-app';
   task:string;
   loader:Boolean = false;
-  alltask:any;
-  constructor(private _todo:TodoserviceService){}
+  alltask=[];
+
+
+  constructor(private _fireservice:AngularFirestore){}
+
   ngOnInit(){
-    this.alltask = this._todo.get_alltask()
+    this.get_alltask()
   }
   
+  // to export file as excel
+  export(){                 
+    if(this.alltask.length!=0){
+      let ele = document.getElementById('export');
+    
+      const ws:XLSX.WorkSheet = XLSX.utils.table_to_sheet(ele);
   
+      const wb:XLSX.WorkBook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb,ws,'Sheet1');
+  
+      XLSX.writeFile(wb,'sheet1.xlsx');
+    }
+    else{
+      alert('No. task to export')
+    }
+    
+  }
+
   submit_task(){
     if(this.task){
       let data = {};
       data['task_name']=this.task;
+      this._fireservice.collection('tasks').add(data)
       
-      this._todo.submit_task(data).then(res=>{
-        
-        // console.log(res)
-       
-      }).catch(
-        err=>{
-          console.log(err)
-      })
     }
     else{
       alert('Add a task first.')
     }
+  }
+
+
+  get_alltask(){
+    this._fireservice.collection('tasks')
+    .snapshotChanges()
+    .subscribe(snap=>{
+      let mydata = []
+      snap.forEach(doc=>{
+        console.log(doc.payload.doc.data());
+        mydata.push(doc.payload.doc.data()['task_name'])
+        
+      })
+     this.alltask = mydata;
+    },
+    err=>{
+      console.log(err)
+    })
+    
+   
   }
 }
